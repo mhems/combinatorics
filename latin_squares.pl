84c34a802caa74a441939f6b38390253dcd37447
Matt Hems <mhems1@binghamton.edu>
2015-10-25 16:17:29 -0400

    initial latin square implementation

--- latin_squares.pl
+++ latin_squares.pl
@@ -12,7 +12,7 @@ max([H|T], Z) :- max(T, Z), H =< Z.
 
 % uniq/1
 uniq([]).
-uniq([H|T]) :- \+ member(H,T), !, uniq(T).
+uniq([H|T]) :- \+ member(H,T), uniq(T).
 
 % range/1
 range([]).
@@ -20,9 +20,18 @@ range(L) :- uniq(L), min(L,1), max(L,N), !, length(L, N).
 
 % square/2
 square([H|T], L) :- length(H, L), square-aux([H|T], L, L).
+
+% square-aux/3
 square-aux([], _, 0).
-square-aux([L|T], W, H) :- length(L, W), square-aux(T, W, Z), H is Z + 1.
+square-aux([L|T], W, H) :- length(L, W), square-aux(T, W, Z), !, H is Z + 1.
 
 % isLatinSquare/1
+isLatinSquare([H|T]) :- length(H, L), range(H), square([H|T], L), L1 is L - 1, allColsUniq([H|T], L1).
 
-% latinSquare/1
+% allColsUniq/2
+allColsUniq(_, 0).
+allColsUniq(S, L) :- getNthCol(S, L, R1), uniq(R1), L1 is L - 1, allColsUniq(S, L1), !.
+
+% getNthCol/3
+getNthCol([], _, []).
+getNthCol([H|T], Index, [Z|R]) :- nth0(Index, H, Z), getNthCol(T, Index, R).
